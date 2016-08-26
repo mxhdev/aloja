f762ac37838341c5fc86f73856e6227e6dac7ee6
timo <timoei@users.noreply.github.com>
2016-08-26 10:50:56 +0200

    New property file (s2rdf), delete s2rdf folder
    - Now new property file of s2rdf cluster for querying the statements
    with JDBC4RDF is used.
    - Deletion of the sparql folder in common_s2rdf was removed. The whole
    s2rdf folder is now removed at the beginning of script.
    - minor format changes of the scripts.

--- shell/common/benchmark_s2rdf.sh
+++ shell/common/benchmark_s2rdf.sh
@@ -37,7 +37,7 @@
 
 
 # Scale factor
-bsbm_products=100000
+bsbm_products=20
 scale_ub=0.5
 exec_engine=hive
 
@@ -50,7 +50,7 @@ spark_master=yarn
 source_file "$ALOJA_REPO_PATH/shell/common/common_s2rdf.sh"
 
 if [ $spark_master == "yarn" ]; then
-	spark_master="yarn --deploy-mode client --driver-memory $driver_memory --executor-cores 3 --num-executors 4 --executor-memory 23G"
+  spark_master="yarn --deploy-mode client --driver-memory $driver_memory --executor-cores 3 --num-executors 4 --executor-memory 23G"
 else 
   spark_master="local[*] --driver-memory $driver_memory"
 fi
@@ -67,17 +67,17 @@ set_spark_requires
 [ ! "$BENCH_LIST" ] && BENCH_LIST="bsbm"
 
 # Delete all files from previous executions
-rm -rf $(get_local_apps_path)/s2rdf
+sudo rm -r $(get_local_apps_path)/s2rdf
 
 # load data and queries
 if [ $bsbm_products == "bsbmrestest" ]; then
-	BENCH_REQUIRED_FILES["S2RDF"]="http://szene-limburg.de/s2rdf/s2rdf_bsbmrestest.tar.gz"
+  BENCH_REQUIRED_FILES["S2RDF"]="http://szene-limburg.de/s2rdf/s2rdf_bsbmrestest.tar.gz"
 elif [ $bsbm_products == "triples" ]; then
-	BENCH_REQUIRED_FILES["S2RDF"]="http://szene-limburg.de/s2rdf/s2rdf_triples.tar.gz"
+  BENCH_REQUIRED_FILES["S2RDF"]="http://szene-limburg.de/s2rdf/s2rdf_triples.tar.gz"
 elif [ $bsbm_products = 1000 ]; then
-	BENCH_REQUIRED_FILES["S2RDF"]="http://szene-limburg.de/s2rdf/data1k.tar.gz"
+  BENCH_REQUIRED_FILES["S2RDF"]="http://szene-limburg.de/s2rdf/data1k.tar.gz"
 else
-	BENCH_REQUIRED_FILES["S2RDF"]="http://szene-limburg.de/s2rdf/s2rdf_DataGenerator.tar.gz"
+  BENCH_REQUIRED_FILES["S2RDF"]="http://szene-limburg.de/s2rdf/s2rdf_DataGenerator.tar.gz"
   generate_data=1
 fi
 
@@ -113,46 +113,43 @@ benchmark_prepare_bsbm() {
   local bench_name="${FUNCNAME[0]##*benchmark_}_$exec_engine$bsbm_products"
   logger "INFO: Preparing $bench_name"
 
-	if [ $generate_data ]; then
-  	function_call "generate_bsbm"
-		function_call "translate_bsbm"
-	else
-		# load data and queries in hdfs
-		execute_hadoop_new "$bench_name" "fs -put $(get_local_apps_path)/s2rdf/bsbmrestest /tmp/hive/s2rdf"
-		execute_hadoop_new "$bench_name" "fs -chmod -R 777 /tmp/hive/s2rdf"
-	fi
+  if [ $generate_data ]; then
+    function_call "generate_bsbm"
+	function_call "translate_bsbm"
+  else
+	# load data and queries in hdfs
+	execute_hadoop_new "$bench_name" "fs -put $(get_local_apps_path)/s2rdf/bsbmrestest /tmp/hive/s2rdf"
+	execute_hadoop_new "$bench_name" "fs -chmod -R 777 /tmp/hive/s2rdf"
+  fi
 
   logger "INFO: Dataset size:"
   execute_hadoop_new "$bench_name" "fs -ls /tmp/hive/s2rdf"
-  # $(get_local_apps_path)/s2rdf/bsbmtools-0.2.1/data.tsv /tmp/hive/s2rdf
-  # execute_hadoop_new "$bench_name" "fs -copyToLocal /tmp/hive/s2rdf $(get_local_apps_path)/s2rdf/hdfs_backup"
-  # du -hcs $(get_local_apps_path)/s2rdf/*
 
-	# Copy hive-site.xml to hive and spark conf folder (for hive cli and hive/spark thrift server use same metastore)
+  # Copy hive-site.xml to hive and spark conf folder (for hive cli and hive/spark thrift server use same metastore)
   cp $(get_base_configs_path)/hive1_conf_template/hive-site.xml $(get_local_apps_path)/apache-hive-1.2.1-bin/conf/
-	cp $(get_base_configs_path)/hive1_conf_template/hive-site.xml $(get_local_apps_path)/spark-1.6.1-bin-hadoop2.6/conf/
+  cp $(get_base_configs_path)/hive1_conf_template/hive-site.xml $(get_local_apps_path)/spark-1.6.1-bin-hadoop2.6/conf/
 
   # run hive import script
   execute_hive "$bench_name" "-hiveconf prepath='' -f $(get_local_apps_path)/s2rdf/loadScript.hql" "time"
 
-	logger "INFO: Starting HiveServer2"
+  logger "INFO: Starting HiveServer2"
   if [ $exec_engine == "spark" ]; then
-		# For Spark
-		logger "INFO: Executing with spark"		
-		execute_cmd_master "$bench_name" "cd $(get_local_apps_path)/$SPARK_VERSION/sbin; $(get_spark_exports) ./start-thriftserver.sh --master $spark_master --conf spark.kryoserializer.buffer.max=1024 &"
-	else
-		# For Hive
-		#execute_cmd_master "$bench_name" "$(get_hive_exports) $HIVE_HOME/bin/hive --service hiveserver2 &&"
-		logger "INFO: Executing with hive"
-		local hive_exports="$(get_hive_exports)"
-		local hive_bin="$HIVE_HOME/bin/hive"
-		local hive_cmd="$hive_exports
+	# For Spark
+	logger "INFO: Executing with spark"		
+	execute_cmd_master "$bench_name" "cd $(get_local_apps_path)/$SPARK_VERSION/sbin; $(get_spark_exports) ./start-thriftserver.sh --master $spark_master --conf spark.kryoserializer.buffer.max=1024 &"
+  else
+	# For Hive
+	#execute_cmd_master "$bench_name" "$(get_hive_exports) $HIVE_HOME/bin/hive --service hiveserver2 &&"
+	logger "INFO: Executing with hive"
+	local hive_exports="$(get_hive_exports)"
+	local hive_bin="$HIVE_HOME/bin/hive"
+	local hive_cmd="$hive_exports
 	$hive_bin --service hiveserver2 &"
-		eval $hive_cmd
-	fi
+	eval $hive_cmd
+  fi
 
-	logger "INFO: Wait 60 seconds to get server started..."
-	sleep 60
+  logger "INFO: Wait 60 seconds to get server started..."
+  sleep 60
 
 }
 
@@ -174,13 +171,9 @@ benchmark_bsbm() {
   chmod +rwx $(get_local_apps_path)/s2rdf/jdbc4rdf/jdbc4rdf_0.4.jar
   local bench_name="${FUNCNAME[0]##*benchmark_}_$exec_engine$bsbm_products"
 
-  # edit the config file for increasing the memory. Passing this argument as command line modifier to jdbc4rdf will not work (only the first parameter until ; will be used)
-  ## two >s means append line
-  echo "executor.urisuffix=?mapreduce.map.memory.mb=20000;mapreduce.map.java.opts=-Xmx18000m;mapreduce.reduce.memory.mb=20000;mapreduce.reduce.java.opts=-Xmx18000m" >> $(get_local_apps_path)/s2rdf/jdbc4rdf/jdbc4rdf_vagrant.properties
-
   # default hive credentials: user=s2rdf, password= 
 	# For using spark db.driver=spark has to be changed!
-  execute_cmd_master "$bench_name" "$(get_java_home)/bin/java -jar $(get_local_apps_path)/s2rdf/jdbc4rdf/jdbc4rdf_0.4.jar exec $(get_local_apps_path)/s2rdf/jdbc4rdf/jdbc4rdf_vagrant.properties executor.queryfile=$(get_local_apps_path)/s2rdf/queries.txt db.driver=$exec_engine db.auth.user=s2rdf" "time"
+  execute_cmd_master "$bench_name" "$(get_java_home)/bin/java -jar $(get_local_apps_path)/s2rdf/jdbc4rdf/jdbc4rdf_0.4.jar exec $(get_local_apps_path)/s2rdf/jdbc4rdf/jdbc4rdf_azureA7.properties executor.queryfile=$(get_local_apps_path)/s2rdf/queries.txt db.driver=$exec_engine" "time"
   
 
   logger "DEBUG: Creating backups at scratch/local"
@@ -200,10 +193,8 @@ benchmark_suite_cleanup() {
   clean_hadoop
 
   logger "INFO: Sstopping HiveServer2"
-# kill hiveserver2 since there is no command to stop it...
+  # kill hiveserver2 since there is no command to stop it...
   kill -9 $(ps aux | grep '[S]erver2' | awk '{print $2}')
-  
-  
 }
 
 
--- shell/common/common_s2rdf.sh
+++ shell/common/common_s2rdf.sh
@@ -13,11 +13,11 @@ generate_bsbm() {
   # Creating data with bsbm tool
   execute_cmd_master "$bench_name" "cd $(get_local_apps_path)/s2rdf/bsbmtools-0.2.1; export PATH=$PATH:$(get_java_home)/bin; java -cp bin:lib/* benchmark.generator.Generator -s tsv -sp 20 -pc $bsbm_products -fn data"
 
-	# Transform data in tab seperated values, shorten predicates to 20 characters
+  # Transform data in tab seperated values, shorten predicates to 20 characters
   #execute_cmd_master "$bench_name" "python $(get_local_apps_path)/s2rdf/s2rdfconverter.py -fp $(get_local_apps_path)/s2rdf/bsbmtools-0.2.1/data.nt"
 
-	# Put data file in hdfs
-	execute_hadoop_new "$bench_name" "fs -mkdir -p /tmp/hive/s2rdf"
+  # Put data file in hdfs
+  execute_hadoop_new "$bench_name" "fs -mkdir -p /tmp/hive/s2rdf"
   execute_hadoop_new "$bench_name" "fs -put $(get_local_apps_path)/s2rdf/bsbmtools-0.2.1/data.tsv /tmp/hive/s2rdf"
 
   logger "INFO: HDFS Disk Space"
@@ -51,31 +51,27 @@ generate_bsbm() {
   logger "INFO: HDFS Disk Space"
   show_hdfs_diskspace
 
-	# create references to hive metastore
-	execute_cmd_master "$bench_name" "cp -f ~/loadScript.hql $(get_local_apps_path)/s2rdf"
+  # create references to hive metastore
+  execute_cmd_master "$bench_name" "cp -f ~/loadScript.hql $(get_local_apps_path)/s2rdf"
 
-	# copy statistic files to working directory
+  # copy statistic files to working directory
   execute_cmd_master "$bench_name" "cp -f ~/stat_* $(get_local_apps_path)/s2rdf/stat"
 
-	logger "INFO: Done generating BSBM Data!"
+  logger "INFO: Done generating BSBM Data!"
 
 }
 
 translate_bsbm() {
   logger "INFO: Generating BSBM Queries..."
-	
-	# make sure no old queries are still in the directory
-	execute_cmd_master "$bench_name" "cd $(get_local_apps_path)/s2rdf/sparql; rm -r *"
 
-	# generate sparql queries with bsbm testdriver (no warm up runs, 1 querymix)
-	execute_cmd_master "$bench_name" "cd $(get_local_apps_path)/s2rdf/bsbmtools-0.2.1; export PATH=$PATH:$(get_java_home)/bin; java -cp bin:lib/* benchmark.testdriver.TestDriver -runs 1 -w 0 fp=$(get_local_apps_path)/s2rdf/sparql"
+  # generate sparql queries with bsbm testdriver (no warm up runs, 1 querymix)
+  execute_cmd_master "$bench_name" "cd $(get_local_apps_path)/s2rdf/bsbmtools-0.2.1; export PATH=$PATH:$(get_java_home)/bin; java -cp bin:lib/* benchmark.testdriver.TestDriver -runs 1 -w 0 fp=$(get_local_apps_path)/s2rdf/sparql"
 
-	logger "INFO: Translating SPARQL Queries..."
-	# translate queries with s2rdf translator
-	execute_cmd_master "$bench_name" "cd $(get_local_apps_path)/s2rdf; export PATH=$PATH:$(get_java_home)/bin; python translateBSBM.py -s sparql/ -t sql/ -u $scale_ub"
- # execute_cmd_master "$bench_name" "cd $(get_local_apps_path)/s2rdf; export PATH=$PATH:$(get_java_home)/bin; python translateBSBM.py -s $(get_local_apps_path)/sparql/ -t sql/ -u $scale_ub"
+  logger "INFO: Translating SPARQL Queries..."
+  # translate queries with s2rdf translator
+  execute_cmd_master "$bench_name" "cd $(get_local_apps_path)/s2rdf; export PATH=$PATH:$(get_java_home)/bin; python translateBSBM.py -s sparql/ -t sql/ -u $scale_ub"
 
-	# Copy new query file to main folder
-	execute_cmd_master "$bench_name" "cd $(get_local_apps_path)/s2rdf; cp -f sql/compositeQueryFile.txt ./queries.txt"
+  # Copy new query file to main folder
+  execute_cmd_master "$bench_name" "cd $(get_local_apps_path)/s2rdf; cp -f sql/compositeQueryFile.txt ./queries.txt"
 
 }
